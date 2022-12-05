b6374a62addb83e2cff0155e06fb4b588771a716
vishantvadher <vadhervishant@gmail.com>
2022-12-05 16:29:38 -0500

    Product Model is created

---
+++ app/src/main/java/com/example/mountainviewcafe/addProduct.java
@@ -0,0 +1,55 @@
+package com.example.mountainviewcafe;
+
+public class addProduct {
+
+    private String title, description, image, price, discount;
+
+
+    public addProduct(String title, String description, String image, String price, String discount) {
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+        this.discount = discount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
+}
